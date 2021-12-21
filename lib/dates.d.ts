93ad59ca8047a9f1123f1814f83b733ab2f4109c
raul_abuin <raul.abuin@beseif.com>
2021-12-21 10:38:37 +0100

    add types and change tsconfig.json

---
+++ lib/dates.d.ts
@@ -0,0 +1,51 @@
+import moment from 'moment';
+export declare enum DateFormat {
+    SqlFormat = "YYYY/MM/DD HH:mm:ss",
+    ReadableFormat = "DD/MM/YYYY",
+    ReadableFormatWithHours = "DD/MM/YYYY HH:mm",
+    Timestamp = "",
+    Joined = "YYYYMMDD",
+    Cron = "ss mm HH DD MM E"
+}
+declare type PeriodUnit = `minutes` | `hours` | `days` | `months` | `years`;
+declare type Pattern = DateFormat | string;
+export declare class Dates {
+    static readonly now: (format?: Pattern) => string;
+    static readonly date: (str?: string) => moment.Moment;
+    static readonly format: (date: any, options: {
+        format: Pattern;
+        pattern?: Pattern;
+    }) => string | number;
+    static readonly isInPeriod: (date: any, options: {
+        pattern?: Pattern;
+        period: number;
+        unit: PeriodUnit;
+    }) => boolean;
+    static readonly hasPassed: (date: any, options: {
+        pattern?: Pattern;
+    }) => boolean;
+    static readonly isBefore: (one: {
+        date: any;
+        pattern?: Pattern;
+    }, other: {
+        date: any;
+        pattern?: Pattern;
+    }) => boolean;
+    static readonly getNextWorkDay: () => moment.Moment;
+    static readonly add: (date: any, options: {
+        value: number;
+        unit: PeriodUnit;
+        pattern?: Pattern;
+        format?: Pattern;
+    }) => string | number;
+    static readonly substract: (date: any, options: {
+        value: number;
+        period: PeriodUnit;
+        pattern?: Pattern;
+        format?: Pattern;
+    }) => string | number;
+    static readonly startOf: (period: PeriodUnit, params: {
+        format?: Pattern;
+    }) => string;
+}
+export {};
---
+++ lib/joi.d.ts
@@ -0,0 +1,48 @@
+declare type stringParams = {
+    min?: number;
+    max?: number;
+    length?: number;
+    valid?: string[];
+};
+declare type dateParams = {
+    format?: string;
+};
+declare type emailParams = {
+    min?: number;
+    max?: number;
+};
+declare type numberParams = {
+    min?: number;
+    max?: number;
+};
+declare type arrayParams = {
+    min?: number;
+    max?: number;
+    items?: any;
+};
+declare type alternativeParams = {
+    property?: string;
+    valid?: string[];
+};
+export declare class Joi {
+    static readonly alternatives: (params?: alternativeParams) => any;
+    static readonly string: (params?: stringParams) => any;
+    static readonly requiredString: (params?: stringParams) => any;
+    static readonly date: (params?: dateParams) => any;
+    static readonly requiredDate: (params?: dateParams) => any;
+    static readonly email: (params?: emailParams) => any;
+    static readonly requriedEmail: (params?: emailParams) => any;
+    static readonly number: (params?: numberParams) => any;
+    static readonly requiredNumber: (params?: numberParams) => any;
+    static readonly array: (params?: arrayParams) => any;
+    static readonly requiredArray: (params?: arrayParams) => any;
+    static readonly bool: () => any;
+    static readonly requiredBool: () => any;
+    static readonly object: (schema?: object) => any;
+    static readonly requiredObject: (schema?: object) => any;
+    static readonly allowUnknown: {
+        allowUnknown: boolean;
+    };
+    static readonly validateSchema: (schema: any, params: any, options?: any) => Promise<any>;
+}
+export {};
